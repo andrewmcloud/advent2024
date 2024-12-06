17e3562e6bba9d3b0c5bbbd23b7877475ae91702
Andrew McLoud <amcloud@gmail.com>
2024-12-06 16:25:05 -0500

    day 6 part 2

--- advent2024/day6.py
+++ advent2024/day6.py
@@ -3,8 +3,6 @@ from itertools import cycle
 with open("../input/day6.txt", "r") as f:
     grid = [list(line) for line in f.read().strip().split("\n")]
 
-directions = cycle([(-1, 0), (0, 1), (1, 0), (0, -1)])
-
 
 def find_start(grid):
     for y, line in enumerate(grid):
@@ -14,27 +12,41 @@ def find_start(grid):
     raise Exception("no start")
 
 
-def move(grid, directions):
+def move(grid:list) -> tuple[set, bool]:
+    directions = cycle([(-1, 0), (0, 1), (1, 0), (0, -1)])
     visited = set()
     current_position = find_start(grid)
-    visited.add(current_position)
     direction = next(directions)
+    visited.add((current_position, direction))
 
     while True:
         next_position = (current_position[0] + direction[0], current_position[1] + direction[1])
         if next_position[0] < 0 or next_position[0] > len(grid[0]) - 1 or next_position[1] < 0 or next_position[1] > len(grid) - 1:
-            return visited
+            return visited, False
 
         element = grid[next_position[0]][next_position[1]]
+
         if element == "#":
             direction = next(directions)
         else:
-            visited.add(next_position)
+            if (next_position, direction) in visited:
+                return set(), True
+            visited.add((next_position, direction))
             current_position = next_position
 
 
 # part 1:
-part1 = len(move(grid, directions))
+part1 = {m[0] for m in move(grid)[0]}
 
-print(f"part1: {part1}")
+# part 2
+count = []
+for (i, j) in part1:
+    if grid[i][j] == "#" or grid[i][j] == "^":
+        continue
+    grid[i][j] = "#"
+    count.append(move(grid)[1])
+    grid[i][j] = "."
+part2 = sum(count)
 
+print(f"part1: {len(part1)}")
+print(f"part1: {part2}")
