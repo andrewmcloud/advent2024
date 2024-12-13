747da03332d6ccd4afc5a52f1cf535f39246523f
Andrew McLoud <amcloud@gmail.com>
2024-12-13 17:12:44 -0500

    day 13

--- advent2024/day13.py
+++ advent2024/day13.py
@@ -1,26 +1,30 @@
 import re
-with open ("../input/day13.txt", "r") as f:
+with open("../input/day13.txt", "r") as f:
     input_ = f.read()
 
 games = input_.split("\n\n")
 
 
-def claw_game(make_it_high: bool):
+def claw_game(make_it_high: bool) -> int:
     tokens = 0
     for game in games:
         button_a, button_b, prize = game.split("\n")
 
-        x1, x2 = [int(x) for x in re.findall(r"\d+", button_a)]
-        y1, y2 = [int(x) for x in re.findall(r"\d+", button_b)]
+        x1, y1 = [int(x) for x in re.findall(r"\d+", button_a)]
+        x2, y2 = [int(x) for x in re.findall(r"\d+", button_b)]
         z1, z2 = [int(x) for x in re.findall(r"\d+", prize)]
 
         z1 += 10000000000000 if make_it_high else 0
         z2 += 10000000000000 if make_it_high else 0
 
-        b = int((z2 * x1 - x2 * z1) / (y2 * x1 - x2 * y1))
-        a = int((z1 - b * y1) / x1)
+        # solve equations
+        # x1 * a + x2 * b = z1
+        # y1 * a + y2 * b = z2
 
-        if x1 * a + y1 * b == z1 and x2 * a + y2 * b == z2:
+        b = int((z2 * x1 - y1 * z1) / (y2 * x1 - y1 * x2))
+        a = int((z1 - b * x2) / x1)
+
+        if x1 * a + x2 * b == z1 and y1 * a + y2 * b == z2:
             tokens += 3 * a + b
     return tokens
 
